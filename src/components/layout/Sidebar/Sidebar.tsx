416b1fe9f623f550c04e48a128240cbe5553678f
Eber Junior :) <142128061+eberssj@users.noreply.github.com>
2026-05-25 21:23:54 -0300

    API-167-RF-06-gerar-relatorios-em-PDF para dev
    * feat: sugestões de perguntas no frontend
    
    * feat: gerador de pdf baseado em id de chat (versão inicial)
    
    * feat: gerador de pdf baseado em id de chat (versão inicial)

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -63,10 +63,11 @@ export default function Sidebar() {
     const normalStyle = { color: theme.orange.main };
 
     const menuItems = [
-    { to: "/", label: "Chatbot", icon: <ChatbotIcon />, disabled: false },
-    { to: "/dashboard", label: "Dashboard", icon: <DashboardIcon />, disabled: false },
-    { to: "/filtros", label: "Filtros", icon: <FilterIcon />, disabled: false },
-];
+        { to: "/", label: "Chatbot", icon: <ChatbotIcon />, disabled: false },
+        { to: "/dashboard", label: "Dashboard", icon: <DashboardIcon />, disabled: false },
+        { to: "/relatorio", label: "Relatório", icon: <ReportIcon />, disabled: false },
+        { to: "/filtros", label: "Filtros", icon: <FilterIcon />, disabled: false },
+    ];
 
     return (
         <aside className={styles.sidebar} style={{ background: theme.background.secondary }}>
@@ -157,11 +158,11 @@ const ChatbotIcon = () => (
     </svg>
 );
 const ReportIcon = () => (
-    <svg width="24" height="24" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M26.25 31.5H17.5" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M31.5 24.5H17.5" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M7 38.5H35C35.9283 38.5 36.8185 38.1313 37.4749 37.4749C38.1313 36.8185 38.5 35.9283 38.5 35V7C38.5 6.07174 38.1313 5.1815 37.4749 4.52513C36.8185 3.86875 35.9283 3.5 35 3.5H14C13.0717 3.5 12.1815 3.86875 11.5251 4.52513C10.8687 5.1815 10.5 6.07174 10.5 7V35C10.5 35.9283 10.1313 36.8185 9.47487 37.4749C8.8185 38.1313 7.92826 38.5 7 38.5ZM7 38.5C6.07174 38.5 5.1815 38.1313 4.52513 37.4749C3.86875 36.8185 3.5 35.9283 3.5 35V19.25C3.5 18.3217 3.86875 17.4315 4.52513 16.7751C5.1815 16.1187 6.07174 15.75 7 15.75H10.5" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M29.75 10.5H19.25C18.2835 10.5 17.5 11.2835 17.5 12.25V15.75C17.5 16.7165 18.2835 17.5 19.25 17.5H29.75C30.7165 17.5 31.5 16.7165 31.5 15.75V12.25C31.5 11.2835 30.7165 10.5 29.75 10.5Z" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M15 18h-5"/>
+        <path d="M18 14h-8"/>
+        <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-4 0v-9a2 2 0 0 1 2-2h2"/>
+        <rect width="8" height="4" x="10" y="6" rx="1"/>
     </svg>
 );
 const DashboardIcon = () => (
